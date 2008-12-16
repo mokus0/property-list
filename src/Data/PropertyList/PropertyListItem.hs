69c06bcb006935a735fc004c717e2c0b9f18a063
James Cook <james.cook@usma.edu>
2008-12-16 19:00:57 +0000

    implemented rough cut of key-path based manipulation

--- src/Data/PropertyList/PropertyListItem.hs
+++ src/Data/PropertyList/PropertyListItem.hs
@@ -18,6 +18,7 @@ import Data.PropertyList.Type
 import qualified Data.Map as M
 import Data.ByteString (ByteString)
 import Data.Time
+import Data.Char
 
 import Text.XML.HaXml.OneOfN
 
@@ -36,56 +37,55 @@ class PropertyListItem i where
     -- fail.
     fromPropertyList :: PropertyList -> Maybe i
 
-alterPropertyList :: 
-    (PropertyListItem i, PropertyListItem i') 
-    => (Maybe i -> (Maybe i', a))
-    -> PropertyList -> (PropertyList, a)
-alterPropertyList f plist = case f (fromPropertyList plist) of
-    (plist', a) -> (maybe plist toPropertyList plist', a)
-
+-- this stuff is pretty sketchy at present.  It works, but I don't think I
+-- really like it.  The types and semantics just don't feel right yet.
 alterPropertyListIfPossible ::
     (PropertyListItem i, PropertyListItem i') 
-    => (i -> (i', Maybe a))
-    -> PropertyList -> (PropertyList, Maybe a)
+    => (i -> (Maybe i', a))
+    -> PropertyList -> (Maybe PropertyList, Maybe a)
 alterPropertyListIfPossible f pl = case fmap f (fromPropertyList pl) of
-    Nothing -> (pl, Nothing)
-    Just (i', a) -> (toPropertyList i', a)
+    Nothing -> (Just pl, Nothing)
+    Just (i', a) -> (fmap toPropertyList i', Just a)
 
-alterDictionaryEntry ::
-    (PropertyListItem i, PropertyListItem i') 
-    => String -> (Maybe i -> (Maybe i', Maybe a)) 
-    -> M.Map String PropertyList -> (M.Map String PropertyList, Maybe a)
-alterDictionaryEntry k f dict = case f (M.lookup k dict >>= fromPropertyList) of
-    (entry', a) -> (maybe dict (\e -> M.insert k (toPropertyList e) dict) entry', a)
-
-alterDictionaryEntryIfExists ::
-    (PropertyListItem i, PropertyListItem i') 
-    => String -> (i -> (i', Maybe a)) 
-    -> M.Map String PropertyList -> (M.Map String PropertyList, Maybe a)
-alterDictionaryEntryIfExists k f dict = case M.lookup k dict >>= fromPropertyList of
-    Nothing -> (dict, Nothing)
-    Just item -> case f item of
-        (item, a) -> (M.insert k (toPropertyList item) dict, a)
-    
 alterItemAtKeyPath ::
     (PropertyListItem i, PropertyListItem i') 
-    => [String] -> (i -> (i', Maybe a))
-    -> PropertyList -> (PropertyList, Maybe a)
+    => [String] -> (i -> (Maybe i', a))
+    -> PropertyList -> (Maybe PropertyList, Maybe a)
 alterItemAtKeyPath []     f = alterPropertyListIfPossible f
-alterItemAtKeyPath (k:ks) f = alterPropertyListIfPossible 
-    $ alterDictionaryEntryIfExists k
-    $ alterItemAtKeyPath ks f
-    
+alterItemAtKeyPath (k:ks) f = alterPropertyListIfPossible' 
+    (alterDictionaryEntryIfExists' k
+        (alterItemAtKeyPath ks f))
+    where
+        alterPropertyListIfPossible' ::
+            (PropertyListItem i, PropertyListItem i') 
+            => (i -> (i', Maybe a))
+            -> PropertyList -> (Maybe PropertyList, Maybe a)
+        alterPropertyListIfPossible' f pl = case fmap f (fromPropertyList pl) of
+            Nothing -> (Just pl, Nothing)
+            Just (i', a) -> (Just (toPropertyList i'), a)
+        
+        alterDictionaryEntryIfExists' ::
+            (PropertyListItem i, PropertyListItem i') 
+            => String -> (i -> (Maybe i', Maybe a)) 
+            -> M.Map String PropertyList -> (M.Map String PropertyList, Maybe a)
+        alterDictionaryEntryIfExists' k f dict = case M.lookup k dict >>= fromPropertyList of
+            Nothing -> (dict, Nothing)
+            Just item -> case f item of
+                (item, a) -> (M.update (const (fmap toPropertyList item)) k dict, a)
+
+getPropertyListItemAtKeyPath :: PropertyListItem i =>
+    [String] -> PropertyList -> Maybe i
+getPropertyListItemAtKeyPath path = snd . alterItemAtKeyPath path (\i -> (Just i,i))
 
 instance PropertyListItem PropertyList where
     toPropertyList = id
     fromPropertyList = Just
 
-{-# SPECIALIZE instance PropertyListItem [PropertyList] #-}
-
-instance PropertyListItem a => PropertyListItem [a] where
-    toPropertyList = PLArray . fmap toPropertyList
-    fromPropertyList (PLArray x) = mapM fromPropertyList x
+-- can't make this polymorphic in the list element because
+-- of overlap with 'String'
+instance PropertyListItem [PropertyList] where
+    toPropertyList = PLArray
+    fromPropertyList (PLArray x) = Just x
     fromPropertyList _ = Nothing
 
 instance PropertyListItem ByteString where
@@ -111,21 +111,36 @@ instance PropertyListItem a => PropertyListItem (M.Map String a) where
 instance PropertyListItem Double where
     toPropertyList = PLReal
     fromPropertyList (PLReal d) = Just d
+    fromPropertyList (PLString s) = case reads s of
+        [(d, "")] -> Just d
+        _ -> Nothing
     fromPropertyList _ = Nothing
 
 instance PropertyListItem Integer where
     toPropertyList = PLInt
     fromPropertyList (PLInt i) = Just i
+    fromPropertyList (PLString s) = case reads s of
+        [(i, "")] -> Just i
+        _ -> Nothing
     fromPropertyList _ = Nothing
 
 instance PropertyListItem String where
     toPropertyList = PLString
     fromPropertyList (PLString x) = Just x
+    fromPropertyList (PLBool True) = Just "YES"
+    fromPropertyList (PLBool False) = Just "NO"
+    fromPropertyList (PLInt i) = Just (show i)
+    fromPropertyList (PLReal d) = Just (show d)
     fromPropertyList _ = Nothing
 
 instance PropertyListItem Bool where
     toPropertyList = PLBool
     fromPropertyList (PLBool d) = Just d
+    fromPropertyList (PLString b)
+        | map toLower b `elem` ["yes", "true"]
+        = Just True
+        | map toLower b `elem` ["no", "false"]
+        = Just False
     fromPropertyList _ = Nothing
 
 instance PropertyListItem UnparsedPlistItem where
