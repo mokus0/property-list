856f51216c4239fd32825759315081f59f20aeaf
James Cook <james.cook@usma.edu>
2009-10-09 15:17:02 +0000

    Rewrote parts of Object.hs to not need CPP hackery

--- src/Data/PropertyList/Object.hs
+++ src/Data/PropertyList/Object.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE 
-        FlexibleContexts, UndecidableInstances, CPP
+        FlexibleContexts, UndecidableInstances
   #-}
 module Data.PropertyList.Object ({- instances only -}) where
 
@@ -7,30 +7,25 @@ import Data.Object
 import Data.PropertyList.Type
 import Data.PropertyList.Parse
 import Data.PropertyList.Xml
-#ifdef data_object_uses_lazy_bytestrings
-import Data.ByteString.Lazy.Char8
-#else
-import Data.ByteString.Char8
-#endif
 import qualified Data.Map as M
 import Control.Monad
 import Control.Monad
 import Control.Monad.Error
 
 instance ToScalar UnparsedPlistItem where
-    toScalar = pack . showXml . plistItemToPlist . unparsedPlistItemToPlistItem
+    toScalar = toScalar . showXml . plistItemToPlist . unparsedPlistItemToPlistItem
 instance ToObject UnparsedPlistItem where
     toObject = Scalar . toScalar
 
 instance (ToObject (l a), ToObject (m a)) => ToObject (PropertyListS l m a) where
     toObject (PLArray a)    = toObject a
     toObject (PLData bs)    = toObject bs
-    toObject (PLDate d)     = showToScalar d
+    toObject (PLDate d)     = showToObject d
     toObject (PLDict d)     = toObject d
-    toObject (PLReal d)     = showToScalar d
-    toObject (PLInt i)      = showToScalar i
-    toObject (PLString s)   = stringToScalar s
-    toObject (PLBool b)     = showToScalar b
+    toObject (PLReal d)     = showToObject d
+    toObject (PLInt i)      = showToObject i
+    toObject (PLString s)   = toObject s
+    toObject (PLBool b)     = showToObject b
 
 instance (ToObject (f (M f a)), ToObject a) => ToObject (M f a) where
     toObject (S x) = toObject x
@@ -39,8 +34,8 @@ instance (ToObject (f (M f a)), ToObject a) => ToObject (M f a) where
 instance (ToScalar k, ToObject v) => ToObject (M.Map k v) where
     toObject = toObject . M.assocs
 
-stringToScalar = Scalar . pack
-showToScalar :: Show a => a -> Object
-showToScalar = stringToScalar . show
+showToObject :: Show a => a -> Object
+showToObject = toObject . show
 
+test :: Object
 test = toObject (undefined :: PropertyList)
